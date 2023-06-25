8a2172dee58a73b4c1b473e0861f0432fb7671a6
Jason Ku <jasonku@me.com>
2023-06-26 00:48:33 +0800

    removed gui, added line select, refactor event handlers, added perp through point and 4 point angle bisector

--- src/main.js
+++ src/main.js
@@ -3,11 +3,33 @@ import { NOTE } from "./note.js";
 import { SVG } from "./svg.js";
 import { IO } from "./io.js";
 import { X } from "./conversion.js";
-import { GUI } from "./gui.js";
 
 window.onload = () => { MAIN.startup(); };  // entry point
 
 const MAIN = {
+    color: {
+        background: "lightgray",
+        normal: "black",
+        active: "red",
+        select: "blue",
+        face: {
+            top: "gray",
+            bottom: "white",
+        },
+        edge: {
+            U: "black",
+            F: "lightgray",
+            B: "black",
+        },
+    },
+    opacity: {
+        normal: 0.01,
+        hover: 1,
+    },
+    radius: {
+        normal: 5,
+        hover: 10,
+    },
     startup: () => {
         NOTE.clear_log();
         NOTE.start("*** Starting Flat-Folder ***");
@@ -16,7 +38,7 @@ const MAIN = {
         const main = document.getElementById("main");
         for (const [k, v] of Object.entries({
             xmlns: SVG.NS,
-            style: `background: ${GUI.COLORS.background}`,
+            style: `background: ${MAIN.color.background}`,
             viewBox: [0, 0, 2*s, s].join(" "),
         })) {
             main.setAttribute(k, v);
@@ -123,67 +145,106 @@ const MAIN = {
         const cells = CP.map(V => M.expand(V, Q));
         const colors = tops.map(d => {
             if (d == undefined) { return undefined; }
-            if (Ff[d] != flip)  { return GUI.COLORS.face.top; }
-            else                { return GUI.COLORS.face.bottom; }
+            if (Ff[d] != flip)  { return MAIN.color.face.top; }
+            else                { return MAIN.color.face.bottom; }
         });
         const visible = MAIN.PP_Ctop_CP_SC_2_visible(Q, PP, tops, CP, SC);
         SVG.draw_polygons(svg, cells, {
             id: "fold_c", fill: colors, stroke: colors});
         const lines = SP.map((ps) => M.expand(ps, Q));
         SVG.draw_segments(svg, lines, {
-            id: "fold_s_crease", stroke: GUI.COLORS.edge.F,
+            id: "fold_s_crease", stroke: MAIN.color.edge.F,
             filter: (i) => SD[i] == "C"});
         SVG.draw_segments(svg, lines, {
-            id: "fold_s_edge", stroke: GUI.COLORS.edge.B,
+            id: "fold_s_edge", stroke: MAIN.color.edge.B,
             filter: (i) => SD[i] == "B"});
         const Lsvg = SVG.append("g", svg, {id: "lines"});
-        const [R1, R2] = [5, 10];
-        const [O1, O2] = [0.01, 1];
         SVG.draw_points(svg, Q, {
-            id: "fold_p", fill: GUI.COLORS.line.N, r: R1,
-            filter: (i) => visible[i], opacity: O1,
+            id: "fold_p", filter: (i) => visible[i],
+            fill: MAIN.color.normal, r: MAIN.radius.normal,
+            opacity: MAIN.opacity.normal,
         });
         const clicked = new Map();
         for (let i = 0; i < Q.length; ++i) {
             const el = document.getElementById(`fold_p${i}`);
             if (el != undefined) {
-                el.onmouseover = () => {
-                    el.setAttribute("r", R2);
-                    el.setAttribute("opacity", O2);
-                };
-                el.onmouseout = () => {
-                    el.setAttribute("r", R1);
-                    el.setAttribute("opacity", (clicked.get(i) == undefined)
-                        ? O1
-                        : O2
-                    );
-                };
-                el.onclick = () => {
-                    NOTE.time(`Clicked point ${i}`);
-                    if (clicked.has(i) || (clicked.size > 2)) {
-                        NOTE.time(`Emptying clicked set`);
-                        SVG.clear("lines");
-                        for (const [i, el] of clicked) {
-                            el.setAttribute("fill", GUI.COLORS.line.N);
-                            el.setAttribute("opacity", O1);
-                        }
-                        clicked.clear();
-                        return;
-                    }
-                    clicked.set(i, el);
-                    NOTE.time(`Clicked set is: [${clicked.keys()}]`);
-                    el.setAttribute("fill", GUI.COLORS.line.A);
-                    SVG.clear("lines");
-                    const L = MAIN.get_lines(Array.from(clicked.keys()).map(i => Q[i]));
-                    if (L.length > 0) {
-                        SVG.draw_segments(Lsvg, L.map(l => MAIN.line_2_coords(l)), {
-                            id: "line", stroke: GUI.COLORS.line.L, stroke_width: R1,
-                        });
-                    }
-                };
+                el.onmouseover = () => MAIN.point_over(el);
+                el.onmouseout = () => MAIN.point_out(i, el, clicked);
+                el.onclick = () => MAIN.point_click(i, el, clicked, Lsvg, Q);
             }
         }
         NOTE.lap();
+    },
+    point_over: (el) => {
+        el.setAttribute("fill", MAIN.color.select);
+        el.setAttribute("r", MAIN.radius.hover);
+        el.setAttribute("opacity", MAIN.opacity.hover);
+    },
+    point_out: (i, el, clicked) => {
+        const unclicked = (clicked.get(i) == undefined);
+        el.setAttribute("r", MAIN.radius.normal);
+        el.setAttribute("opacity", unclicked
+            ? MAIN.opacity.normal
+            : MAIN.opacity.hover
+        );
+        el.setAttribute("fill", unclicked
+            ? MAIN.color.normal
+            : MAIN.color.active
+        );
+    },
+    point_click: (i, el, clicked, svg, Q) => {
+        NOTE.time(`Clicked point ${i}`);
+        if (clicked.has(i) || (clicked.size > 3)) {
+            SVG.clear("lines");
+            MAIN.clear_clicked(clicked);
+            MAIN.point_over(el);
+            return;
+        }
+        clicked.set(i, el);
+        NOTE.time(`Clicked set is: [${Array.from(clicked.keys())}]`);
+        el.setAttribute("fill", MAIN.color.active);
+        SVG.clear("lines");
+        const L = MAIN.get_lines(Array.from(clicked.keys()).map(i => Q[i]));
+        if (L.length > 0) {
+            const g = SVG.draw_segments(svg, L.map(l => MAIN.line_2_coords(l)), {
+                id: "line", stroke: MAIN.color.normal,
+                stroke_width: MAIN.radius.normal,
+            });
+            for (const el of g.children) {
+                el.onclick = () => MAIN.line_click(el, clicked);
+                el.onmouseover = () => MAIN.line_over(el);
+                el.onmouseout = () => MAIN.line_out(el);
+            }
+        } else if (clicked.size > 1) {
+            SVG.clear("lines");
+            MAIN.clear_clicked(clicked);
+            MAIN.point_over(el);
+        }
+    },
+    clear_clicked: (clicked) => {
+        for (const [i, el] of clicked) {
+            el.setAttribute("fill", MAIN.color.normal);
+            el.setAttribute("opacity", MAIN.opacity.normal);
+        }
+        clicked.clear();
+    },
+    line_over: (el) => {
+        el.setAttribute("stroke", MAIN.color.select);
+        el.setAttribute("stroke-width", MAIN.radius.hover);
+    },
+    line_out: (el) => {
+        el.setAttribute("stroke", MAIN.color.normal);
+        el.setAttribute("stroke-width", MAIN.radius.normal);
+    },
+    line_click: (el, clicked) => {
+        SVG.clear("lines");
+        MAIN.clear_clicked(clicked);
+        document.getElementById("lines").appendChild(el);
+        el.onmouseover = undefined;
+        el.onmouseout = undefined;
+        el.onclick = undefined;
+        el.setAttribute("stroke", MAIN.color.active);
+        el.setAttribute("stroke-width", MAIN.radius.normal);
     },
     FOLD_2_V_FV_FO: (doc) => {
         const ex = JSON.parse(doc);
@@ -237,31 +298,57 @@ const MAIN = {
         if (P.length == 2) {
             const [a, b] = P;
             const m = M.div(M.add(a, b), 2);
-            const v1 = M.sub(b, a);
-            const v2 = M.perp(v1);
+            const v1 = M.sub(b, a); // line through AB
+            const v2 = M.perp(v1);  // perpendicular bisector of AB
             for (let v of [v1, v2]) {
-                const ang = M.angle(v);
                 const u = M.unit(v);
                 const d = M.dot(m, u);
-                out.push([ang, d]);
+                out.push([u, d]);
             }
         } else if (P.length == 3) {
             const [a, b, c] = P;
-            if (Math.abs(M.area2(a, b, c)) > M.FLOAT_EPS) {
-                const ba = M.unit(M.sub(a, b));
-                const bc = M.unit(M.sub(c, b));
-                const u = M.unit(M.add(ba, bc));
-                const v = M.perp(u);
-                const ang = M.angle(v);
-                const d = M.dot(b, v);
-                out.push([ang, d]);
+            if (Math.abs(M.area2(a, b, c)) > 0.000001) {
+                {   // angle bisector of ABC
+                    const ba = M.unit(M.sub(a, b));
+                    const bc = M.unit(M.sub(c, b));
+                    const u = M.unit(M.add(ba, bc));
+                    const v = M.perp(u);
+                    const d = M.dot(b, v);
+                    out.push([v, d]);
+                }
+                {   // through A perpendicular to BC
+                    const u = M.unit(M.sub(c, b));
+                    const d = M.dot(a, u);
+                    out.push([u, d]);
+                }
+            }
+        } else if (P.length == 4) {
+            const [a, b, c, d] = P; // angle bisector of AB and CD
+            const ba = M.unit(M.sub(a, b));
+            const cd = M.unit(M.sub(d, c));
+            const perp = M.perp(cd);
+            const parallel = Math.abs(M.dot(perp, ba)) < 0.000001;
+            const u = parallel ? ba : M.unit(M.add(ba, cd));
+            const U = parallel ? [u] : [u, M.perp(u)];
+            for (const v of U) {
+                const pc = M.dot(c, v);
+                const pa = M.dot(a, v);
+                const pb = M.dot(b, v);
+                const bb = M.add(a, M.mul(M.sub(b, a), (pc - pa)/(pb - pa)));
+                const x = M.div(M.add(bb, c), 2);
+                const pv = M.perp(v);
+                const [aL, bL, cL, dL] = [a, b, c, d].map(p => {
+                    return M.dot(M.sub(p, x), pv) < 0;
+                });
+                if ((aL != bL) || (bL != cL) || (cL != dL)) {
+                    out.push([pv, M.dot(pv, x)]);
+                }
             }
         }
         return out;
     },
     line_2_coords: (L) => {
-        const [a, d] = L;
-        const u = [Math.cos(a), Math.sin(a)];
+        const [u, d] = L;
         const p = M.mul(u, d);
         const off = M.mul(M.perp(u), 10);
         const p1 = M.add(p, off);
